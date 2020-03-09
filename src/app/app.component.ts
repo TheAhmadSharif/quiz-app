be572081c02ba159ce1bda41ce249b6c96ba7046
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-03-09 18:55:13 +0600

    Quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-app';
+  username:any;
+  userEmail:any;
+  mark: any;
+  userObject = [];
+  user = {
+    name: 'Ahmad',
+    email: 'TheAhmadSharif@gmail.com',
+    answer: [],
+    mark: []
+  }
+  answer:any;
+  
+  items;
+  constructor(firestore: AngularFirestore) {
+    firestore.collection('quiz').valueChanges().subscribe(object => {
+
+      this.items = object;
+      
+      console.log(object);
+
+      
+    });
+    
+  }
+
+  omSubmit(event:any, user) {
+    event.preventDefault();
+
+    console.log(user);
+    var answer = ["Orange", "Dhaka"];
+
+    this.user.mark = [];
+
+    
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'
 import { NgModule } from '@angular/core';
+
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
