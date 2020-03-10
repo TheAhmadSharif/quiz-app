e91ab2aeee1a4cc4d27878d69cbb290964ea0d46
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-03-10 17:50:22 +0600

    validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,20 @@ import 'firebase/firestore';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  username:any;
-  userEmail:any;
+
   items:any;
   idList = [];
   userObject = [];
   user = {
-    name: 'Ahmad',
-    email: 'TheAhmadSharif@gmail.com',
-    answer: [],
-    mark: []
+    name: '',
+    email: '',
   }
   answer = ["Orange", "Dhaka"];
+  publishscore: boolean;
+  score: number = 0;
   
   
-  constructor(firestore: AngularFirestore) {
+  constructor(public firestore: AngularFirestore) {
     firestore.collection('quiz').valueChanges().subscribe(object => {
 
       this.items = object;
@@ -42,31 +41,39 @@ export class AppComponent implements OnInit {
   getUserValue(question_no:number, option_no:number, useranswer:string) {
       this.idList[question_no] = 'mark-' + question_no.toString() + option_no.toString(); 
       this.userObject[question_no] = useranswer;
-      // console.log(this.idList);
       
   }
 
   omSubmit(event:any, user) {
+    this.publishscore = true;
     event.preventDefault();
+    var element = document.getElementById("mark-*");
 
    for(var i = 0; i < this.userObject.length; i++) {
     let id = this.idList[i];
 
     if(this.answer[i] == this.userObject[i]) {
         document.getElementById(id).classList.add("correctMark");
+        this.score = this.score + 1;
     }
     else {
         document.getElementById(id).classList.add("wrongMark");
     }
-
-   
-    
-
    }
+    var email = user.email;
 
-    console.log(this.userObject);
+    console.log(user, this.score);
 
+     this.firestore.collection('quiz_answer').doc(email).set({
+        username: user.name,
+        email: user.email,
+        score: this.score
+     })
 
+  }
+
+  refreshPage() {
+    location.reload();
 
   }
 
